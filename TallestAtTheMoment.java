52e57c6e6c154dc53f583ee74da52799d3483ff2
“surendhar149” <surendhar.l149@gmail.com>
2026-09-02 22:21:55 +0530

    Added tallest at the moment

---
+++ TallestAtTheMoment.java
@@ -0,0 +1,26 @@
+import java.util.*;
+// 111524202055 Surendhar L
+public class TallestAtTheMoment {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int takeshis = scan.nextInt();
+        Map<Integer,Integer> map = new TreeMap<>();
+        for(int i=0;i<takeshis;i++){
+            int heightT = scan.nextInt();
+            int timeT = scan.nextInt();
+            map.put(heightT,timeT);
+        }
+        int[] height = new int[takeshis];
+        int[] time = new int[takeshis];
+        for(Map.Entry<Integer,Integer> ety : map.entrySet()){
+            
+        }
+        
+        List<Integer> h = new LinkedList<>();
+        List<Integer> t = new LinkedList<>();
+        
+        int querries = scan.nextInt();
+        int arr[] = new int[querries];
+        for(int i=0;i<)
+    }
+}
